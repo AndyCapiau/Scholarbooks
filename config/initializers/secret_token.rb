c8a0c9460d4f8dcae0fcfdade4ebefdb166a3b63
Andy Capiau <andy.capiau@gmail.com>
2011-09-11 08:37:52 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scholarbooks::Application.config.secret_token = 'ed3c270d05926307e628ff05e9d84ff6416ea7986b0eabd4fb45e16c916622f091c41f789975aeed814bfa7937bab84693334e2d41aab47e426651e31ae878d3'
